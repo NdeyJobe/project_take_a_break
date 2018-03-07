1fb6e3561125cc85eab9cfb4d5e38794ff9da42e
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-07 17:17:55 +0100

    Add looping condition

---
+++ break_time.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+count = 0
+while (count < 100):
+  #to make my program wait
+  time.sleep(10)
+  
+
+  #to open a web browser
+  webbrowser.open("https://www.youtube.com/watch?v=S6-uf4ri3Qs")
+
+  count =+ 1 + 20
