80fe31943ae75a7f76446e8a1e4612ad59a0ead4
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-09 13:29:30 +0100

    Add sleep module

--- break_time.py
+++ break_time.py
@@ -1,13 +1,14 @@
 import webbrowser
 import time
 
-count = 0
-while (count < 100):
-  #to make my program wait
-  time.sleep(10)
-  
+total_breaks = 3
+break_count = 0
 
+print("This program started on " , time.ctime())
+while (break_count < total_breaks):
+#to make my program wait
+  time.sleep(7200)
   #to open a web browser
-  webbrowser.open("https://www.youtube.com/watch?v=S6-uf4ri3Qs")
-
-  count =+ 1 + 20
+  webbrowser.open("https://www.youtube.com/watch?v=EM7Cp7qZiR4")
+  time.sleep(7200)
+  break_count =+ 1
