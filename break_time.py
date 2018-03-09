dc3d7d974c69a0bf8a0ff8b1b0d4fb661bc1fc4b
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-09 14:34:30 +0100

    add time lapse

--- break_time.py
+++ break_time.py
@@ -4,11 +4,10 @@ import time
 total_breaks = 3
 break_count = 0
 
-print("This program started on " , time.ctime())
+print("This program started on " + time.ctime())
 while (break_count < total_breaks):
-#to make my program wait
-  time.sleep(7200)
+#to make the program wait
+  time.sleep(7200) 
   #to open the web browser
-  webbrowser.open("https://www.youtube.com/watch?v=EM7Cp7qZiR4")
-  time.sleep(7200)
+  webbrowser.open("https://www.youtube.com/watch?v=izGwDsrQ1eQ")
   break_count =+ 1
