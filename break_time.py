a1bccb66e8ccf36ec27f0d352fce1f2d8945ba40
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-09 13:32:26 +0100

    Add sleep function

--- break_time.py
+++ break_time.py
@@ -8,7 +8,7 @@ print("This program started on " , time.ctime())
 while (break_count < total_breaks):
 #to make my program wait
   time.sleep(7200)
-  #to open a web browser
+  #to open the web browser
   webbrowser.open("https://www.youtube.com/watch?v=EM7Cp7qZiR4")
   time.sleep(7200)
   break_count =+ 1
